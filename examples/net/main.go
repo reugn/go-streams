0a7c6111d75b435b126939220739255a07cf5d29
reugn <reugpro@gmail.com>
2019-11-30 14:23:28 +0200

    add network connector example

---
+++ examples/net/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/reugn/go-streams"
+	ext "github.com/reugn/go-streams/extension"
+	"github.com/reugn/go-streams/flow"
+)
+
+// Test producer: nc -u 127.0.0.1 3434
+// Test consumer: nc -u -l 3535
+func main() {
+	source, err := ext.NewNetSource(ext.UDP, "127.0.0.1:3434")
+	streams.Check(err)
+	flow1 := flow.NewMap(toUpper, 1)
+	sink, err := ext.NewNetSink(ext.UDP, "127.0.0.1:3535")
+	streams.Check(err)
+
+	source.Via(flow1).To(sink)
+}
+
+var toUpper = func(in interface{}) interface{} {
+	msg := in.(string)
+	fmt.Println("Got: %s", msg)
+	return strings.ToUpper(msg)
+}

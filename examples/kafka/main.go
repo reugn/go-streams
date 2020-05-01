0b4dd0a4cfdda8ddbda0b4dcd600bb2de7819c5d
reugn <reugpro@gmail.com>
2020-05-01 17:01:10 +0300

    migrate from Confluent to Sarama kafka client

--- examples/kafka/main.go
+++ examples/kafka/main.go
@@ -1,42 +1,42 @@
 package main
 
 import (
+	"context"
 	"os"
 	"os/signal"
 	"strings"
 	"syscall"
 	"time"
 
-	"github.com/confluentinc/confluent-kafka-go/kafka"
+	"github.com/Shopify/sarama"
 	ext "github.com/reugn/go-streams/extension"
 	"github.com/reugn/go-streams/flow"
 )
 
 func main() {
-	host := "127.0.0.1:9092"
-	config := kafka.ConfigMap{
-		"bootstrap.servers":  host,
-		"group.id":           "myGroup",
-		"enable.auto.commit": false,
-		"auto.offset.reset":  "earliest",
-	}
+	hosts := []string{"127.0.0.1:9092"}
+	ctx := context.Background()
+	config := sarama.NewConfig()
+	config.Consumer.Group.Rebalance.Strategy = sarama.BalanceStrategyRoundRobin
+	config.Consumer.Offsets.Initial = sarama.OffsetNewest
+	config.Producer.Return.Successes = true
+	config.Version, _ = sarama.ParseKafkaVersion("2.2.0")
+	groupID := "testConsumer"
 
-	source := ext.NewKafkaSource(&config, "test")
+	source := ext.NewKafkaSource(ctx, hosts, groupID, config, "test")
 	flow1 := flow.NewMap(toUpper, 1)
 	flow2 := flow.NewFlatMap(appendAsterix, 1)
-	sink := ext.NewKafkaSink(&config, "test2")
+	sink := ext.NewKafkaSink(hosts, config, "test2")
 	throttler := flow.NewThrottler(1, time.Second*1, 50, flow.Discard)
 	// slidingWindow := flow.NewSlidingWindow(time.Second*30, time.Second*5)
 	tumblingWindow := flow.NewTumblingWindow(time.Second * 5)
-	//manual offset commit flow
-	commit := source.Commit()
 
-	source.Via(flow1).Via(commit).Via(throttler).Via(tumblingWindow).Via(flow2).To(sink)
+	source.Via(flow1).Via(throttler).Via(tumblingWindow).Via(flow2).To(sink)
 	wait()
 }
 
 var toUpper = func(in interface{}) interface{} {
-	msg := in.(*kafka.Message)
+	msg := in.(*sarama.ConsumerMessage)
 	msg.Value = []byte(strings.ToUpper(string(msg.Value)))
 	return msg
 }
@@ -45,7 +45,7 @@ var appendAsterix = func(in interface{}) []interface{} {
 	arr := in.([]interface{})
 	rt := make([]interface{}, len(arr))
 	for i, item := range arr {
-		msg := item.(*kafka.Message)
+		msg := item.(*sarama.ConsumerMessage)
 		msg.Value = []byte(string(msg.Value) + "*")
 		rt[i] = msg
 	}

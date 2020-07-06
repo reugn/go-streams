21ac19dd4772f86c350867cc65177b796e6cac9d
reugn <reugpro@gmail.com>
2020-07-06 23:11:17 +0300

    update documentation

--- extension/chan.go
+++ extension/chan.go
@@ -5,38 +5,38 @@ import (
 	"github.com/reugn/go-streams/flow"
 )
 
-// ChanSource streams data from chan
+// ChanSource streams data from the input channel
 type ChanSource struct {
 	in chan interface{}
 }
 
-// NewChanSource returns new ChanSource instance
+// NewChanSource returns a new ChanSource instance
 func NewChanSource(in chan interface{}) *ChanSource {
 	return &ChanSource{in}
 }
 
-// Via streams data through given flow
+// Via streams data through the given flow
 func (cs *ChanSource) Via(_flow streams.Flow) streams.Flow {
 	flow.DoStream(cs, _flow)
 	return _flow
 }
 
-// Out returns channel for sending data
+// Out returns an output channel for sending data
 func (cs *ChanSource) Out() <-chan interface{} {
 	return cs.in
 }
 
-// ChanSink transmitts data to chan
+// ChanSink sends data to the output channel
 type ChanSink struct {
 	Out chan interface{}
 }
 
-// NewChanSink returns new ChanSink instance
+// NewChanSink returns a new ChanSink instance
 func NewChanSink(out chan interface{}) *ChanSink {
 	return &ChanSink{out}
 }
 
-// In returns channel for receiving data
+// In returns an input channel for receiving data
 func (ch *ChanSink) In() chan<- interface{} {
 	return ch.Out
 }

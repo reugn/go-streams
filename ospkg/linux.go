8e8be8049b850065285b27a4a1974ffbaaebdaca
reugn <reugpro@gmail.com>
2020-07-07 14:33:20 +0300

    use platform specific constants

---
+++ ospkg/linux.go
@@ -0,0 +1,11 @@
+// +build !windows
+
+package ospkg
+
+const (
+	// PathSeparator is a path separator string for a Unix-like os
+	PathSeparator = "/"
+
+	// NewLine is a new line constant for a Unix-like os
+	NewLine = "\n"
+)

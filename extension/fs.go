aedbeffe371b5a5d02191ef41564b5de70b9127b
reugn <reugpro@gmail.com>
2021-11-22 09:16:19 +0200

    log and exit on a file operation error

--- extension/fs.go
+++ extension/fs.go
@@ -2,23 +2,26 @@ package extension
 
 import (
 	"bufio"
+	"log"
 	"os"
 
 	"github.com/reugn/go-streams"
 	"github.com/reugn/go-streams/flow"
-	"github.com/reugn/go-streams/util"
 	"github.com/reugn/go-streams/util/ospkg"
 )
 
-// FileSource streams data from the file
+// FileSource represents an inbound connector that reads items from a file.
 type FileSource struct {
 	fileName string
 	in       chan interface{}
 }
 
-// NewFileSource returns a new FileSource instance
+// NewFileSource returns a new FileSource instance.
 func NewFileSource(fileName string) *FileSource {
-	source := &FileSource{fileName, make(chan interface{})}
+	source := &FileSource{
+		fileName: fileName,
+		in:       make(chan interface{}),
+	}
 	source.init()
 	return source
 }
@@ -26,7 +29,9 @@ func NewFileSource(fileName string) *FileSource {
 func (fs *FileSource) init() {
 	go func() {
 		file, err := os.Open(fs.fileName)
-		util.Check(err)
+		if err != nil {
+			log.Fatalf("FileSource failed to open the file %s", fs.fileName)
+		}
 		defer file.Close()
 		reader := bufio.NewReader(file)
 		for {
@@ -59,15 +64,18 @@ func (fs *FileSource) Out() <-chan interface{} {
 	return fs.in
 }
 
-// A FileSink writes items to a file
+// FileSink represents an outbound connector that writes items to a file.
 type FileSink struct {
 	fileName string
 	in       chan interface{}
 }
 
-// NewFileSink returns a new FileSink instance
+// NewFileSink returns a new FileSink instance.
 func NewFileSink(fileName string) *FileSink {
-	sink := &FileSink{fileName, make(chan interface{})}
+	sink := &FileSink{
+		fileName: fileName,
+		in:       make(chan interface{}),
+	}
 	sink.init()
 	return sink
 }
@@ -75,11 +83,15 @@ func NewFileSink(fileName string) *FileSink {
 func (fs *FileSink) init() {
 	go func() {
 		file, err := os.OpenFile(fs.fileName, os.O_CREATE|os.O_WRONLY, 0600)
-		util.Check(err)
+		if err != nil {
+			log.Fatalf("FileSink failed to open the file %s", fs.fileName)
+		}
 		defer file.Close()
 		for elem := range fs.in {
 			_, err = file.WriteString(elem.(string))
-			util.Check(err)
+			if err != nil {
+				log.Fatalf("FileSink failed to write to the file %s", fs.fileName)
+			}
 		}
 	}()
 }

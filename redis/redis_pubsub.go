68478ed7e3af64b5be599ef8185cf269d66e0774
Eugene R <reugpro@gmail.com>
2024-05-14 11:32:00 +0300

    refactor(redis): minor connector improvements (#132)

--- redis/redis_pubsub.go
+++ redis/redis_pubsub.go
@@ -16,111 +16,112 @@ import (
 // Rather, published messages are characterized into channels, without knowledge
 // of what (if any) subscribers there may be.
 type PubSubSource struct {
-	ctx         context.Context
 	redisClient *redis.Client
 	channel     string
 	out         chan any
 }
 
+var _ streams.Source = (*PubSubSource)(nil)
+
 // NewPubSubSource returns a new PubSubSource instance.
 //
 // The given redisClient is subscribed to the provided channel.
-// The replies to subscription and unsubscribing operations are sent in the form of messages
-// so that the client reads a coherent stream of messages where the first element
-// indicates the type of message.
-func NewPubSubSource(ctx context.Context, redisClient *redis.Client, channel string) (*PubSubSource, error) {
+// The replies to subscription and unsubscribing operations are sent in the form
+// of messages so that the client reads a coherent stream of messages where the
+// first element indicates the type of message.
+func NewPubSubSource(ctx context.Context, redisClient *redis.Client,
+	channel string) (*PubSubSource, error) {
 	pubsub := redisClient.Subscribe(ctx, channel)
 
-	// Wait for a confirmation that subscription is created before publishing anything
+	// wait for a confirmation that subscription is created before
+	// publishing anything
 	_, err := pubsub.Receive(ctx)
 	if err != nil {
 		return nil, err
 	}
 
 	source := &PubSubSource{
-		ctx:         ctx,
 		redisClient: redisClient,
 		channel:     channel,
 		out:         make(chan any),
 	}
+	go source.init(ctx, pubsub.Channel())
 
-	go source.init(pubsub.Channel())
 	return source, nil
 }
 
-// init starts the main loop
-func (ps *PubSubSource) init(ch <-chan *redis.Message) {
+func (ps *PubSubSource) init(ctx context.Context, ch <-chan *redis.Message) {
 loop:
 	for {
 		select {
-		case <-ps.ctx.Done():
+		case <-ctx.Done():
 			break loop
-
+		// route incoming messages downstream
 		case msg := <-ch:
 			ps.out <- msg
 		}
 	}
-
-	log.Printf("Closing Redis Pub/Sub consumer")
+	log.Printf("Closing Redis PubSubSource connector")
 	close(ps.out)
-	ps.redisClient.Close()
+	if err := ps.redisClient.Close(); err != nil {
+		log.Printf("Error in Close: %s", err)
+	}
 }
 
-// Via streams data through the given flow
-func (ps *PubSubSource) Via(_flow streams.Flow) streams.Flow {
-	flow.DoStream(ps, _flow)
-	return _flow
+// Via streams data to a specified operator and returns it.
+func (ps *PubSubSource) Via(operator streams.Flow) streams.Flow {
+	flow.DoStream(ps, operator)
+	return operator
 }
 
-// Out returns an output channel for sending data
+// Out returns the output channel of the PubSubSource connector.
 func (ps *PubSubSource) Out() <-chan any {
 	return ps.out
 }
 
 // PubSubSink represents a Redis Pub/Sub sink connector.
 type PubSubSink struct {
-	ctx         context.Context
 	redisClient *redis.Client
 	channel     string
 	in          chan any
 }
 
+var _ streams.Sink = (*PubSubSink)(nil)
+
 // NewPubSubSink returns a new PubSubSink instance.
 //
-// The incoming messages will be published to the given target channel using the
-// provided redis.Client.
-func NewPubSubSink(ctx context.Context, redisClient *redis.Client, channel string) *PubSubSink {
+// The incoming messages will be published to the given target channel using
+// the provided redis.Client.
+func NewPubSubSink(ctx context.Context, redisClient *redis.Client,
+	channel string) *PubSubSink {
 	sink := &PubSubSink{
-		ctx:         ctx,
 		redisClient: redisClient,
 		channel:     channel,
 		in:          make(chan any),
 	}
+	go sink.init(ctx)
 
-	go sink.init()
 	return sink
 }
 
-// init starts the main loop
-func (ps *PubSubSink) init() {
+func (ps *PubSubSink) init(ctx context.Context) {
 	for msg := range ps.in {
-		switch m := msg.(type) {
+		switch message := msg.(type) {
 		case string:
-			err := ps.redisClient.Publish(ps.ctx, ps.channel, m).Err()
-			if err != nil {
-				log.Printf("Error in redisClient.Publish: %s", err)
+			if err := ps.redisClient.Publish(ctx, ps.channel, message).Err(); err != nil {
+				log.Printf("Error in Publish: %s", err)
 			}
-
 		default:
-			log.Printf("Unsupported message type %v", m)
+			log.Printf("Unsupported message type: %T", message)
 		}
 	}
-
-	log.Printf("Closing Redis Pub/Sub producer")
-	ps.redisClient.Close()
+	log.Printf("Closing Redis PubSubSink connector")
+	if err := ps.redisClient.Close(); err != nil {
+		log.Printf("Error in Close: %s", err)
+	}
 }
 
-// In returns an input channel for receiving data
+// In returns the input channel of the PubSubSink connector.
 func (ps *PubSubSink) In() chan<- any {
 	return ps.in
 }

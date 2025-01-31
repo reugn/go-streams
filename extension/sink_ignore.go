05bdc469d857431594b9b58832a04539508494df
Eugene R. <reugpro@gmail.com>
2025-01-31 11:33:15 +0200

    chore: reorganize the extension package (#155)

---
+++ extension/sink_ignore.go
@@ -0,0 +1,42 @@
+package extension
+
+import "github.com/reugn/go-streams"
+
+// IgnoreSink represents a simple outbound connector that discards
+// all elements of a stream.
+type IgnoreSink struct {
+	in chan any
+}
+
+var _ streams.Sink = (*IgnoreSink)(nil)
+
+// NewIgnoreSink returns a new IgnoreSink connector.
+func NewIgnoreSink() *IgnoreSink {
+	ignoreSink := &IgnoreSink{
+		in: make(chan any),
+	}
+
+	// asynchronously process stream data
+	go ignoreSink.process()
+
+	return ignoreSink
+}
+
+func (ignore *IgnoreSink) process() {
+	for {
+		_, ok := <-ignore.in
+		if !ok {
+			break
+		}
+	}
+}
+
+// In returns the input channel of the IgnoreSink connector.
+func (ignore *IgnoreSink) In() chan<- any {
+	return ignore.in
+}
+
+// AwaitCompletion is a no-op for the IgnoreSink.
+func (ignore *IgnoreSink) AwaitCompletion() {
+	// no-op
+}
